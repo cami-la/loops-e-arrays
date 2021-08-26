4e5d57fd1806ebb2be6157322c4dab8161d7c85a
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2021-08-26 18:20:29 -0300

    Fatorial

--- src/br/com/dio/exercicios/loops/Ex6_Fatorial.java
+++ src/br/com/dio/exercicios/loops/Ex6_Fatorial.java
@@ -1,7 +1,26 @@
 package br.com.dio.exercicios.loops;
+
+import java.util.Scanner;
+
 /*
 Faça um programa que calcule o fatorial de um número inteiro fornecido pelo usuário.
-Ex.: 5!=5.4.3.2.1=120
+Ex.: 5!= 120 (5 X 4 X 3 X 2 X 1)
 */
 public class Ex6_Fatorial {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Fatorial: ");
+        int fatorial = scan.nextInt();
+
+        int multiplicacao = 1;
+
+        System.out.print(fatorial +"! = ");
+        for(int i = fatorial ; i >= 1 ; i --) {
+            multiplicacao = multiplicacao * i;
+        }
+
+        System.out.println(multiplicacao);
+    }
+
 }
